ed8dca1508c17a8b0254d4ce1af9b252a88aab4f
chungquang <chungducquang2@gmail.com>
2026-03-26 16:40:44 +0700

    fix(webhook): inject PrismaModule into WebhookModule

--- src/modules/webhook/webhook.module.ts
+++ src/modules/webhook/webhook.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WebhookController],
   providers: [WebhookService],
 })
